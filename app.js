74c529e2451cc1a4908d609344a2c5d8e6c0c2da
MCavieres <maca.c.91@gmail.com>
2024-06-21 18:39:53 -0400

    se empaqueta todo en funciones

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const numRandom = randomNumber();
+let numRandom = randomNumber();
 let intentos = 1;
 
 function signText(element, text) {
@@ -14,18 +14,41 @@ function userTry() {
             ".texto__parrafo",
             `Acertaste el número en ${intentos} ${intentos === 1 ? "intento" : "intentos"} `
         );
+        const btnRestart = document.querySelector("#reiniciar");
+        btnRestart.removeAttribute("disabled");
     } else if (userNumber > numRandom) {
         signText(".texto__parrafo", "SkyNet eligio un número menor");
+        cleanInput();
         intentos++;
     } else {
         signText(".texto__parrafo", "SkyNet eligio un número mayor");
+        cleanInput();
         intentos++;
     }
+}
+
+function initCondit() {
+    signText("h1", "Juego adivinar número");
+    signText(".texto__parrafo", "Indica un número del 1 al 10");
+    intentos = 1;
+    numRandom = randomNumber();
+}
+
+function restart() {
+    cleanInput();
+    const btnRestart = document.querySelector("#reiniciar");
+    btnRestart.setAttribute("disabled", "");
+
+    initCondit();
+}
+
+function cleanInput() {
+    const userValue = document.querySelector("#userValue");
+    userValue.value = "";
 }
 
 function randomNumber() {
     return Math.floor(Math.random() * 10) + 1;
 }
 
-signText("h1", "Juego adivinar número");
-signText(".texto__parrafo", "Indica un número del 1 al 10");
+initCondit();
